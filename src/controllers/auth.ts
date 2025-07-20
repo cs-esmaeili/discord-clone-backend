5c8a376692d98364ecae46074be280848d4d50a5
Javad Esmaeili <cs.esmaeili@gmail.com>
2025-07-20 18:16:00 +0330

    google auth done

--- src/controllers/auth.ts
+++ src/controllers/auth.ts
@@ -4,6 +4,8 @@ export const googleCallback = (req: Request, res: Response, next: NextFunction) 
     // JWT را از done می‌گیریم
     const token = req.user as string;
 
+    console.log("log is done token is = " + token);
+
     // اینجا می‌تونی به فرانت ریدایرکت کنی و توکن رو بهش بدی
     res.redirect(`http://localhost:3000/auth/success?token=${token}`);
 };
